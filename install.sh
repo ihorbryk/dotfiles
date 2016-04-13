d07bd2942754b48b24aba04a55929901de7538c8
ihorbryk <ihorbryk@gmail.com>
2016-04-13 11:24:23 +0300

    Add install script

---
+++ install.sh
@@ -0,0 +1,23 @@
+echo "Set caps lock as ctrl"
+setxkbmap -layout us -option ctrl:nocaps
+
+echo "Install git"
+sudo apt-get install -y git
+
+echo "Set git preference"
+git config --global user.name "ihorbryk"
+git config --global user.email "ihorbryk@gmail.com"
+git config --global core.editor "vim"
+
+
+echo "Install vim"
+sudo apt-get install -y vim
+
+echo "Install vandle for vim"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "Copy dot files"
+cp .vimrc ~/
+cp .bashrc ~/
+
+echo "Done!"
